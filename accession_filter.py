ef4511e649f6e6462a97f17cddf6bdc5b1ee655d
ubhuiyan <90285310+ubhuiyan@users.noreply.github.com>
2022-09-09 11:49:06 -0400

    Create accession_filter.py

---
+++ accession_filter.py
@@ -0,0 +1,12 @@
+#%%
+import pandas as pd 
+import numpy as np
+accessions = pd.read_csv('/Users/urnishabhuiyan/Desktop/b4referencefilter.csv') #enter file path 
+accessions.head()
+# %%
+uaccessions = accessions["Accession"].drop_duplicates() #drops duplicated accessions in column 
+# uaccessions.count()
+
+with open("unique_accessions.txt", 'w') as output: #generates a txt list of unique accessions 
+    for row in uaccessions:
+        output.write(str(row) + '\n')
