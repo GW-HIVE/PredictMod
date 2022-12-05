9c25210f1e873380da57d2dbe17778ed4c5b5039
Julia <2963958976zyc@gmail.com>
2022-12-05 09:34:02 -0500

    django program

---
+++ predictmod/models.py
@@ -0,0 +1,6 @@
+from django.db import models
+from django.forms import fields
+
+# Create your models here.
+# class fileinfo(models.Model):
+#     files = fields.FileField()
---
+++ predictmod/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render, HttpResponse, redirect
+import os
+import matlab.engine 
+
+
+# Create your views here.
+def index(request):
+    if request.method == 'GET':
+        return render(request, 'index.html')
+    if request.method == 'POST':
+        files = request.FILES.get("files", None)
+        if not files:
+            return HttpResponse("no files for upload!")
+        destination = open(os.path.join("predictmod/upload/", files.name), 'wb+')
+        for chunk in files.chunks():
+            destination.write(chunk)
+        destination.close()
+        return HttpResponse("Done!")
+
