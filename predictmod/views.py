8fdb6999d14079e88d232d2f3ad34acfaf597c89
Hadley King <hadley_king@gwu.edu>
2022-12-08 13:32:17 -0500

    saving

--- predictmod/views.py
+++ predictmod/views.py
@@ -12,16 +12,20 @@ def index(request):
     if request.method == 'GET':
         return render(request, 'index.html')
     if request.method == 'POST':
-        files = request.FILES.get("files", None)
-        if not files:
-            return HttpResponse("no files for upload!")
-        destination = open(os.path.join("predictmod/upload/", files.name), 'wb+')
-        for chunk in files.chunks():
-            destination.write(chunk)
-        destination.close()
-        eng = matlab.engine.start_matlab()
-        eng.cd(f'{settings.BASE_DIR}/predictmod/', nargout=0)
-        eng.editted_single_predict(nargout=0)
-        result = eng.editted_single_predict()
-        return HttpResponse(result)
+        try:
+            files = request.FILES.get("files", None)
+            if not files:
+                return HttpResponse("no files for upload!")
+            destination = open(os.path.join("predictmod/upload/", files.name), 'wb+')
+            for chunk in files.chunks():
+                destination.write(chunk)
+            destination.close()
+            eng = matlab.engine.start_matlab()
+            MATLAB_FILE = f'{settings.BASE_DIR}/predictmod/{files.name}'
+            eng.cd(f'{settings.BASE_DIR}/predictmod/', nargout=0)
+            eng.editted_single_predict(MATLAB_FILE, nargout=0)
+            result = eng.editted_single_predict()
+            return HttpResponse(result)
+        except Exception as error:
+            return HttpResponse(error)
 
