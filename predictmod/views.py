69f18fb423bb7ece021c3ab08f2784a2750904d4
Pat McNeely <pmcneely@gwu.edu>
2023-04-21 16:11:07 -0400

    wip - views needs some cleanup but this is working preferring PM code in Matlab server development.

--- predictmod/views.py
+++ predictmod/views.py
@@ -2,26 +2,44 @@ from django.shortcuts import render, HttpResponse, redirect
 
 from django.conf import settings
 
-import os
-import matlab.engine 
-import matlab
+import logging
+logging.basicConfig(level=logging.DEBUG, format="%(message)s")
 
+import os
+import getpass
+import requests
+
+# import matlab.engine 
+# import matlab
+
+logger = logging.getLogger()
 
 # Create your views here.
 def index(request):
     if request.method == 'GET':
         return render(request, 'index.html')
     if request.method == 'POST':
-        files = request.FILES.get("files", None)
-        if not files:
-            return HttpResponse("no files for upload!")
-        destination = open(os.path.join("predictmod/upload/", files.name), 'wb+')
-        for chunk in files.chunks():
-            destination.write(chunk)
-        destination.close()
-        eng = matlab.engine.start_matlab()
-        eng.cd(f'{settings.BASE_DIR}/predictmod/', nargout=0)
-        eng.editted_single_predict(nargout=0)
-        result = eng.editted_single_predict()
-        return HttpResponse(result)
+        try:
+            # files = request.FILES.get("files", None)
+            # if not files:
+            #     return HttpResponse("no files for upload!")
+            # try:
+            #     with open(os.path.join("/hostfs", files.name), 'wb+') as fp:
+            #         [fp.write(c) for c in files.chunks()]
+            #     fp.close()
+            # except Exception as e:
+            #     logger.error(f"Caught exception {e}!")
+            #     return HttpResponse(f"User: {getpass.getuser()}")
+            # eng = matlab.engine.start_matlab()
+            # MATLAB_FILE = f'{settings.BASE_DIR}/predictmod/{files.name}'
+            # eng.cd(f'{settings.BASE_DIR}/predictmod/', nargout=0)
+            # eng.editted_single_predict(MATLAB_FILE, nargout=0)
+            # result = eng.editted_single_predict()
+            logger.debug("+"*80)
+            logger.debug(f"Request files: {request.FILES}")
+            logger.debug("+"*80)
+            result = requests.post(f"http://host.docker.internal:5000/", files=request.FILES)
+            return HttpResponse(result)
+        except Exception as error:
+            return HttpResponse(f"Django error:\n\t{error}")
 
