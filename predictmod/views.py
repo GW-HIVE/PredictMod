640c1c3de052bdfaeb7c3b2ef5456d702500886b
Pat McNeely <pmcneely@gwu.edu>
2023-04-27 15:27:05 -0400

    update template with (hacky, hardcoded :/) address that Apache will understand

--- predictmod/views.py
+++ predictmod/views.py
@@ -1,24 +1,27 @@
 from django.shortcuts import render, HttpResponse, redirect
-
+from django.template.response import TemplateResponse
 from django.conf import settings
 
 import logging
+
 logging.basicConfig(level=logging.DEBUG, format="%(message)s")
 
 import os
 import getpass
 import requests
 
-# import matlab.engine 
+# import matlab.engine
 # import matlab
 
 logger = logging.getLogger()
 
+
 # Create your views here.
 def index(request):
-    if request.method == 'GET':
-        return render(request, 'index.html')
-    if request.method == 'POST':
+    if request.method == "GET":
+        args = {"PREDICTMOD_INSTANCE": "/gfkb/predictmod/result/"}
+        return TemplateResponse(request, "index.html", args)
+    if request.method == "POST":
         try:
             # files = request.FILES.get("files", None)
             # if not files:
@@ -35,11 +38,12 @@ def index(request):
             # eng.cd(f'{settings.BASE_DIR}/predictmod/', nargout=0)
             # eng.editted_single_predict(MATLAB_FILE, nargout=0)
             # result = eng.editted_single_predict()
-            logger.debug("+"*80)
+            logger.debug("+" * 80)
             logger.debug(f"Request files: {request.FILES}")
-            logger.debug("+"*80)
-            result = requests.post(f"http://host.docker.internal:5000/", files=request.FILES)
+            logger.debug("+" * 80)
+            result = requests.post(
+                f"http://host.docker.internal:4243/", files=request.FILES
+            )
             return HttpResponse(result)
         except Exception as error:
             return HttpResponse(f"Django error:\n\t{error}")
-
