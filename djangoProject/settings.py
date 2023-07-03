188f9d4527d2b74f75d6fe7f59ec67d3c11779d6
Pat McNeely <pmcneely@gwu.edu>
2023-07-03 14:05:48 -0400

    merge Resolving/burninating more conflicts.

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -1,8 +1,8 @@
 from pathlib import Path
-import os
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-print(BASE_DIR)
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
@@ -14,6 +14,7 @@ SECRET_KEY = "django-insecure-_8*wnfzz(n*6!k3j-@5^&ana+q!)w1!9*bo*6^&qj)2pd825_#
 DEBUG = True
 # Change to *
 ALLOWED_HOSTS = []
+
 
 # Application definition
 
