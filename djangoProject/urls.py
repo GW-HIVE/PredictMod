9d8bb399af4a1a806ce653351006730a0527423e
Pat McNeely <pmcneely@gwu.edu>
2023-07-03 13:49:56 -0400

    update django infrastructure Continued burnination

--- djangoProject/urls.py
+++ djangoProject/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, re_path
+from django.conf import settings
+from django.conf.urls.static import static
 from predictmod import views
 
 from rest_framework import permissions
@@ -47,4 +49,4 @@ urlpatterns = [
     path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     re_path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     re_path(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
