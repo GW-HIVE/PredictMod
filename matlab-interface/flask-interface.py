114a9b5b86364aee7d58f50ed74136c50f9e3d79
Pat McNeely <pmcneely@gwu.edu>
2023-07-05 12:30:31 -0400

    Update the Flask interface to run the decision tree

--- matlab-interface/flask-interface.py
+++ matlab-interface/flask-interface.py
@@ -3,8 +3,17 @@ from flask import Flask, request, Response
 import os
 import time
 import logging
+import pandas as pd
+import pickle
 
-import matlab.engine
+ENGINE = False
+try:
+    import matlab.engine
+
+    ENGINE = True
+except Exception:
+    pass
+
 
 OPERATING_MODE = os.environ.get("OPERATING_MODE", "local")
 
@@ -16,28 +25,65 @@ else:
     PREDICTMOD_APPLICATION_PATH = os.path.expanduser("~/gwu-src/PredictMod/predictmod/")
 
 FILE_HOLDING = os.path.expanduser("~/tmp/")
-ALLOWED_EXTENSIONS = {"xlsx", "xls"}
+EHR_ALLOWED_EXTENSIONS = {"xlsx", "xls"}
+MG_ALLOWED_EXTENSIONS = {
+    "csv",
+}
 
 
-class MatlabRunner:
+class MetagenomicPredictor:
     def __init__(self, path):
-        eng = matlab.engine.start_matlab()
-        eng.cd(PREDICTMOD_APPLICATION_PATH, nargout=0)
-        # eng.load("Synth_data_trained_net.mat")
-        eng.addpath(PREDICTMOD_APPLICATION_PATH)
-        eng.cd(path, nargout=0)
-        # self.nets = [eng.workspace['net1'], eng.workspace['net2'], eng.workspace['net3']]
-        self.engine = eng
+        self.working_path = os.path.abspath(path)
+        try:
+            with open("pickled_tree.pickle", "rb") as fp:
+                self.tree = pickle.load(fp)
+            fp.close()
+        except Exception as e:
+            print(
+                "Warning: Error loading pickle file. Metagenomic predictions disabled!"
+                f"\nError was {e}"
+            )
+            self.tree = None
 
     def make_prediction(self, filename):
-        return self.engine.single_predict(filename)
+        if not self.tree:
+            return f"Error: Metagenomic predictions have been disabled"
+        sample = pd.read_csv(os.path.join(self.working_path, filename))
+        sample = sample.drop(["Status"], axis=1)
+        return f"Prediction based on metagenomic profile: {self.tree.predict(sample)}"
 
 
-runner = MatlabRunner(FILE_HOLDING)
+class EHRMatlabPredictor:
+    def __init__(self, path):
+        if ENGINE:
+            eng = matlab.engine.start_matlab()
+            eng.cd(PREDICTMOD_APPLICATION_PATH, nargout=0)
+            # eng.load("Synth_data_trained_net.mat")
+            eng.addpath(PREDICTMOD_APPLICATION_PATH)
+            eng.cd(path, nargout=0)
+            # self.nets = [eng.workspace['net1'], eng.workspace['net2'], eng.workspace['net3']]
+            self.engine = eng
+        else:
+            self.engine = None
+
+    def make_prediction(self, filename):
+        return (
+            self.engine.single_predict(filename)
+            if self.engine
+            else "MatLab is not currently available"
+        )
 
 
-def allowed_filename(filename):
-    return filename.split(".")[-1] in ALLOWED_EXTENSIONS
+ehr_matlab_predictor = EHRMatlabPredictor(FILE_HOLDING)
+metagenomic_predictor = MetagenomicPredictor(FILE_HOLDING)
+
+
+def ehr_allowed_filename(filename):
+    return filename.split(".")[-1] in EHR_ALLOWED_EXTENSIONS
+
+
+def mg_allowed_filename(filename):
+    return filename.split(".")[-1] in MG_ALLOWED_EXTENSIONS
 
 
 def restarting_the_engine_is_slow(path, file_name):
@@ -56,7 +102,7 @@ app.logger.setLevel(logging.DEBUG)
 FUNCTION_TESTING = False
 
 
-def outstr(str1, str2=None):
+def ehr_outstr(str1, str2=None):
     if FUNCTION_TESTING:
         return f"""
 <b>Received response:</b>
@@ -69,6 +115,7 @@ Function-based result:
 """
     else:
         return f"""
+<h2>Electronic Health Record Analysis Pipeline</h2>
 <b>Received response:</b>
 <br>
     {str1}
@@ -76,31 +123,46 @@ Function-based result:
 """
 
 
+def metagenomic_outstr(str1):
+    return f"""
+<h2>Metagenomic Analysis Pipeline</h2>
+<b>Received response:</b>
+<br>
+    Patient prediction is: {str1}
+<br>
+"""
+
+
 @app.route("/mg-upload", methods=["POST"])
 def mg_request():
-    output_str = "<h1>Meta-genomic prediction toolchain coming soon!</h1><br>"
-    if "files" not in request.files:
-        return Response(f"{output_str}  - Error: No file uploaded.", status=400)
-    else:
+    try:
+        if "files" not in request.files:
+            return Response(
+                f"Metagenomic Analysis - Error! No file uploaded.", status=400
+            )
         file = request.files["files"]
-        return Response(f"{output_str} - Recieved file {file.filename}", status=200)
+        if not file.filename or not mg_allowed_filename(file.filename):
+            return Response(f"<b>Illegal filename {file.filename}!</b>")
+        file.save(os.path.join(FILE_HOLDING, file.filename))
+    except Exception as e:
+        return Response(f"Got an error!\n\t{e}")
+
+    prediction = metagenomic_predictor.make_prediction(file.filename)
+    return Response(metagenomic_outstr(prediction))
 
 
 @app.route("/ehr-upload", methods=["POST"])
 def request_received():
     try:
         if "files" not in request.files:
-            keys_list = ",".join([fk for fk in request.files.keys()])
-            return Response(
-                f"<b>No file received!</b><br>Files was {keys_list}", status=400
-            )
+            return Response("EHR Analysis - Error! No file uploaded.", status=400)
         file = request.files["files"]
-        if not file.filename or not allowed_filename(file.filename):
+        if not file.filename or not ehr_allowed_filename(file.filename):
             return Response(f"<b>Illegal filename {file.filename}!</b>")
         file.save(os.path.join(FILE_HOLDING, file.filename))
 
         obj_start = time.time()
-        prediction = runner.make_prediction(file.filename)
+        prediction = ehr_matlab_predictor.make_prediction(file.filename)
         obj_elapsed = f"{time.time() - obj_start:.2f}s"
         obj_pred = f"{prediction} - {obj_elapsed}"
         func_start = time.time()
@@ -111,10 +173,7 @@ def request_received():
         else:
             func_pred = None
 
-        # app.logger.debug("-" * 80)
-        # app.logger.debug(f"---> Runner output: {prediction}")
-        # app.logger.debug("-" * 80)
-        return Response(outstr(obj_pred, func_pred))
+        return Response(ehr_outstr(obj_pred, func_pred))
 
     except Exception as e:
         return Response(f"Got an error!\n\t{e}")
