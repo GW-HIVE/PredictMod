d6f2535886d47b665c09bce7107d9c2070d33f42
Pat McNeely <pmcneely2@gmail.com>
2023-07-17 10:45:09 -0400

    Deprecate Vue in favor of static service (for now, at least)

---
+++ js/scripts/vue-warning.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "================================================================================"
+echo "NB: Vue services are currently disabled. See 'djangoProject' for relevant files"
+echo "================================================================================"
+
+# Replaced commands:
+#     "serve": "vue-cli-service serve",
+#     "build": "vue-cli-service build",
+#     "lint": "vue-cli-service lint"
+
