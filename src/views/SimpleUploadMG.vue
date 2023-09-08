db93debc3e40138187da2fdf5d7af6b67ffa4f0d
Pat McNeely <pmcneely@gwu.edu>
2023-09-08 15:03:55 -0400

    Remove deprecated buttons

--- src/views/SimpleUploadMG.vue
+++ src/views/SimpleUploadMG.vue
@@ -94,59 +94,6 @@
   </div>
 </v-col>
 
-<form @submit.prevent="sendFile" enctype="multipart/form-data">
-<div class="head font-weight-bold"> 
-    Analyze Metagenomic data
-    </div>
-<div class="field">
-<div class="file is-boxed is-primary is-centered" v-if="message == !'File has been uploaded'">
-    <label class="file-label">
-        <input
-        type="file"
-        ref="MG"
-        @change="selectFile"
-        class="file-input"
-        >
-        <span class="file-cta">
-            <span class="file-icon">
-                <i class="fas fa-upload"></i>
-            </span>
-            <div class="span file-label">
-                Choose a file...
-            </div>
-        </span>
-        <span v-if="file" class="file-name">{{file.name}}</span>
-        <div v-if="message"
-        :class="`message ${error ? 'is-danger' : 'is-success'}`"
-    >
-        <div class="message-body">{{message}}</div>
-    </div>
-    
-    </label>
-</div>
-    </div>
-    <div class="field">
-        <button class="button is-info is-centered" v-if="message ==!'File has been uploaded'">Submit</button>
-    </div>
-    <div class="field">
-        <button class="button is-primary" v-if="message =='File has been uploaded'" @click="show = !show">
-        Show Results
-        </button>
-    </div>
-    <div class="field">
-        <button class="button is-info" v-if="message =='File has been uploaded'" @click="revertFile">
-        Upload a different file
-        </button>
-    </div>
-    <div class="field">
-        <v-card-text class="text-left" v-if="show && message=='File has been uploaded'">
-            <GetMGoutput/>
-        </v-card-text>
-        <v-btn elevation="0" border>
-          <v-list-item href="http://localhost:3344/example-MG"> Example data </v-list-item>
-        </v-btn>
-    </div>
-</form>
 </template>
 
 <script>
