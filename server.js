5d3d6f65b4452f4a69df64ad815bd99fbaeee62f
Pat McNeely <pmcneely@gwu.edu>
2023-09-11 16:40:56 -0400

    Begin moving service to express/Node in favor of deprecating Django (gracefully handles Vite/Vue applications, where Django currently does not)

---
+++ server.js
@@ -0,0 +1,9 @@
+//e.g server.js
+const express = require("express");
+const ViteExpress = require("vite-express");
+
+const app = express();
+
+app.get("/message", (_, res) => res.send("Hello from express!"));
+
+ViteExpress.listen(app, 3000, () => console.log("Server is listening..."));
