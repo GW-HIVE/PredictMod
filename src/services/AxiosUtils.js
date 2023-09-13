4896f93d43fa224970be41ef5d6eabd3cc3b0e4e
Pat McNeely <pmcneely@gwu.edu>
2023-09-13 13:40:49 -0400

    WIP - prepare for deployment to the "tst" server

--- src/services/AxiosUtils.js
+++ src/services/AxiosUtils.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://localhost:4243",
+  baseURL: "http://hivelab.tst.biochemistry.gwu.edu:4245",
   headers: {
     "Content-type": "application/json"
   }
