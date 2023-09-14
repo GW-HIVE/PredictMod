5a52f15ecc94a74347a7f7801c6c77aa7d860882
Pat McNeely <pmcneely@gwu.edu>
2023-09-14 14:58:34 -0400

    Allow export of static URLs for GUnicorn to handle service

--- django_server/server/urls.py
+++ django_server/server/urls.py
@@ -16,7 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", include("ui.urls")),
 ]
+
+urlpatterns += staticfiles_urlpatterns()
