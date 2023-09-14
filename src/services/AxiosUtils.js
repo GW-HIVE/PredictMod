8c313bdba9dcb6400aeb884ea752c1fdbbf76ac8
Pat McNeely <pmcneely@gwu.edu>
2023-09-14 14:24:17 -0400

    Remove artifact and original source files from top level

--- src/services/AxiosUtils.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-export default axios.create({
-  baseURL: "http://hivelab.tst.biochemistry.gwu.edu:4245",
-  headers: {
-    "Content-type": "application/json"
-  }
-});
--- src/views/NotFound.vue
+++
@@ -1,9 +0,0 @@
-<!-- Vue.Js -->
-<!-- NotFound.vue -->
-<template>
-    <div>
-      <h1>
-    Sorry, this page doesn't exist or has been moved to another location
-      </h1>
-    </div>
-</template>
