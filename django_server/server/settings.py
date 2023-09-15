c1cf2734eac1e567d2717addc59588060bc3785b
Pat McNeely <pmcneely@gwu.edu>
2023-09-15 07:56:44 -0400

    Enable logging in the server

--- django_server/server/settings.py
+++ django_server/server/settings.py
@@ -27,6 +27,19 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "handlers": {
+        "console": {
+            "class": "logging.StreamHandler",
+        },
+    },
+    "root": {
+        "handlers": ["console"],
+        "level": "DEBUG",
+    },
+}
 
 # Application definition
 
