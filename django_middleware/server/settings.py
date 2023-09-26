64b5ddbbeac5577bcbef35a6b5fe1a59bab68556
Pat McNeely <pmcneely@gwu.edu>
2023-09-26 15:33:57 -0400

    Open up middleware destinations

--- django_middleware/server/settings.py
+++ django_middleware/server/settings.py
@@ -28,12 +28,14 @@ SECRET_KEY = 'django-insecure-v$rywutoth&6+v8&_#t@_7l*d=k-u^lnx@1)f(o9rs(2pu=i=n
 DEBUG = False
 
 ALLOWED_HOSTS = [
-    "middleware",
-    "127.0.0.1",
-    "localhost"
+    # "middleware",
+    # "127.0.0.1",
+    # "localhost"
+    "*",
 ]
 
 CORS_ALLOWED_ORIGINS = [
+    "middleware",
     "localhost",
     "127.0.0.1",
     "predict",
