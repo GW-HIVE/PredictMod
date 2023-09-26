fbb3d43595621059351d7819e309d0684c94882e
Pat McNeely <pmcneely@gwu.edu>
2023-09-26 15:34:23 -0400

    Open django target URLs to allow lots of endpoints

--- django_middleware/ui/urls.py
+++ django_middleware/ui/urls.py
@@ -4,8 +4,12 @@ from . import views
 
 urlpatterns = [
     path("mg-upload/", views.mg_upload, name="mg-upload"),
+    path("predictmod-beta/mg-upload/", views.mg_upload, name="pm-mg-upload"),
     path("ehr-upload/", views.ehr_upload, name="ehr-upload"),
+    path("predictmod-beta/ehr-upload/", views.ehr_upload, name="pm-ehr-upload"),
     path("mg-sample/", views.mg_sample, name="mg-sample"),
+    path("predictmod-beta/mg-sample/", views.mg_sample, name="pm-mg-sample"),
     path("ehr-sample/", views.ehr_sample, name="ehr-sample"),
+    path("predictmod-beta/ehr-sample/", views.ehr_sample, name="pm-ehr-sample"),
     path("ping/", views.ping, name="ping"),
 ]
