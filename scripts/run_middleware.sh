34a12af0b17da26e0752e99ff4b2b1f217ed3951
Pat McNeely <pmcneely@gwu.edu>
2023-11-28 10:50:32 -0500

    Add development variable options to the build scripts

--- scripts/run_middleware.sh
+++ scripts/run_middleware.sh
@@ -1,3 +1,23 @@
 #!/bin/bash
 
-docker run -d --restart=always --network predictmod  --name middleware predictmod:v0.3.1-middleware
+source ./env.sh
+
+if [[ ! $BUILD_ENV ]]; then
+    echo No build environment found - Aborting.
+    exit 0
+fi
+
+case $BUILD_ENV in
+    'shell_testing'|'development')
+    echo Build environment found $BUILD_ENV. Launch middleware manually.
+    ;;
+    'production'|'beta')
+    docker run -d --restart=always --network predictmod  --name middleware predictmod:v0.3.1-middleware
+    ;;
+    'docker-dev')
+    docker run -d --restart=always -p 8000:8000 --network predictmod  --name middleware predictmod:v0.3.1-middleware
+    ;;
+    *)
+    echo Please update your "env.sh" file appropriately.
+    ;;
+esac
