5c720d3e37244cd8396e041a7285220cbec2c969
Pat McNeely <pmcneely@gwu.edu>
2024-02-29 12:47:47 -0500

    Update URLs with new views.

--- django_middleware/users/urls.py
+++ django_middleware/users/urls.py
@@ -1,28 +1,29 @@
 from django.urls import path, include
-from django.contrib.auth.models import User
-from rest_framework import routers, serializers, viewsets
-
-# from . import views
 
 
-# Serializers define the API representation.
-class UserSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = User
-        fields = ["url", "first_name", "last_name", "email", "is_staff"]
+from . import views
 
+import logging
 
-# ViewSets define the view behavior.
-class UserViewSet(viewsets.ModelViewSet):
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
+logger = logging.getLogger()
 
 
 # Routers provide an easy way of automatically determining the URL conf.
-router = routers.DefaultRouter()
-router.register(r"users", UserViewSet)
+# router = routers.DefaultRouter()
+# router.register(r"", UserViewSet)
+# router.register(r"login", views.login_view)
+# router.register(r"whoami", views.whoami_view)
 
 urlpatterns = [
-    path("", include(router.urls)),
+    # path("api/users/", include(router.urls)),
+    path("api/users/", views.UserListView.as_view()),
+    path("api/create-user/", views.CreateUser.as_view()),
+    path("api/update-user/", views.UpdateUser.as_view()),
+    path("api/delete-user/", views.DeleteUser.as_view()),
+    path("api/login/", views.login_view),
+    path("api/logout/", views.logout_view),
+    path("api/whoami/", views.whoami_view),
+    # path("api/csrf/", views.get_csrf),
+    # path("api/session/", views.session_view),
     # path("api-auth/", include("rest_framework.urls", namespace="rest_framework")),
 ]
