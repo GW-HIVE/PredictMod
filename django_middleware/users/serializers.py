2c672dfd315c3ba91dc3af581ed5ea0682947e15
Pat McNeely <pmcneely@gwu.edu>
2024-02-29 12:45:53 -0500

    Update URLs to direct to Django API endpoints

---
+++ django_middleware/users/serializers.py
@@ -0,0 +1,18 @@
+from django.contrib.auth.models import User
+
+from rest_framework import serializers
+
+
+# Serializers define the API representation.
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = [
+            "id",
+            "username",
+            "first_name",
+            "last_name",
+            "email",
+            "groups",
+            "is_staff",
+        ]
