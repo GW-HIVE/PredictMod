f8b37602b8582be7db21c670574a01a08e75b168
Pat McNeely <pmcneely@gwu.edu>
2024-03-27 11:51:03 -0400

    Update flask interface to support output of PNG data

--- flask_backend/flask_python_interface.py
+++ flask_backend/flask_python_interface.py
@@ -6,9 +6,13 @@ import time
 import json
 import logging
 import pickle
+import io
+from base64 import b64encode
 
 import numpy as np
 import pandas as pd
+import matplotlib
+import matplotlib.pyplot as plt
 import shap
 
 
@@ -34,18 +38,42 @@ class MDClone_EHRTreeHandler:
         fp.close()
         self.pickled_tree = pickled_tree
         self.explainer = shap.Explainer(pickled_tree)
+        # For shipping images
 
     def make_prediction(self, data):
         prediction = self.pickled_tree.predict(data)[0]
         shap_values = self.explainer.shap_values(data)
-        fp = shap.force_plot(
-            self.explainer.expected_value[0], shap_values[0], data.iloc[0]
-        )
         result = "expected" if prediction == "R" else "not expected"
+        # To return an image
+        shap.force_plot(
+            self.explainer.expected_value[0],
+            shap_values[0],
+            data.iloc[0],
+            matplotlib=True,
+            show=False,
+        )
+        buf = io.BytesIO()
+        # XXX?
+        # width, height = plt.gcf().get_size_inches()
+        # app.logger.debug(f"Image dimensions are {height} x {width}")
+        # plt.gcf().set_size_inches(height * 2, width * 2)
+        # app.logger.debug(
+        #     "Image dimensions are NOW {0} x {1}".format(*(plt.gcf().get_size_inches()))
+        # )
+        plt.savefig(buf, format="png", bbox_inches="tight")
+        b64_image = b64encode(buf.getvalue()).decode("utf-8").replace("\n", "")
         return {
             "result": f"This patient is {result} to respond to the intervention based on EHR input",
-            "plot": json.dumps(fp.data),
+            "image": b64_image,
         }
+        # To return JSONified information
+        # fp = shap.force_plot(
+        #     self.explainer.expected_value[0], shap_values[0], data.iloc[0]
+        # )
+        # return {
+        #     "result": f"This patient is {result} to respond to the intervention based on EHR input",
+        #     "plot": json.dumps(fp.data),
+        # }
 
 
 app = Flask(__name__)
