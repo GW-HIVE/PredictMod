ced6c3e483f99824fbcfdf0eb01fb2df08a94837
Pat McNeely <pmcneely@gwu.edu>
2024-04-02 13:41:36 -0400

    Adjust input file title for personnel

--- vuetify_app/src/components/PersonnelContent.vue
+++ vuetify_app/src/components/PersonnelContent.vue
@@ -9,7 +9,7 @@
 // import MarkdownItAnchor from 'markdown-it-anchor';
 
 import { marked } from "marked";
-import Text from '@/docs/about_content/about_UI_v1.2.md?raw';
+import Text from '@/docs/about_content/team_UI_v1.3.md?raw';
 
 export default {
   name: "PersonnelContent",
