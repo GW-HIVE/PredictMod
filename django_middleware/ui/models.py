3e619ff0ae66acadd9d7a55aa8e1f163d3525307
Pat McNeely <pmcneely@gwu.edu>
2024-05-14 13:32:27 -0400

    Initial push to include models in a searchable configuration

--- django_middleware/ui/models.py
+++ django_middleware/ui/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
+
 # Create your models here.
+class ReleasedModel(models.Model):
+    name = models.CharField(max_length=256)
+    version = models.CharField(max_length=24)
+    release_date = models.DateField()
+    data_type = models.CharField(max_length=24)
+    model_type = models.CharField(max_length=256)
+    data_meta = models.JSONField()
+    # A map of the above sets of information into what is
+    # displayed to the user at the WebUI
+    information_mapping = models.JSONField(null=True)
+    additional_details = models.JSONField(null=True)
+    backend = models.URLField()
+
+
+class PendingModel(models.Model):
+    name = models.CharField(max_length=256)
+    version = models.CharField(max_length=24)
+    data_type = models.CharField(max_length=24)
