e4665ab2d45ba4cd8dce9e6e01e68835266a5cda
Pat McNeely <pmcneely@gwu.edu>
2024-05-14 14:42:16 -0400

    Fix user fields to inject working examples

--- django_middleware/utilities/create_users.py
+++ django_middleware/utilities/create_users.py
@@ -1,7 +1,13 @@
 from django.contrib.auth.models import User
 
 user1 = User.objects.create_user(
-    username="Sample User", email="mazumderlab@gwu.edu", password="SampleUser"
+    username="mazumderlab@gwu.edu",
+    email="mazumderlab@gwu.edu",
+    password="SampleUser",
+    first_name="Sample",
+    last_name="User",
 )
 
-user2 = User.objects.create_user(username="p", email="p@p.com", password="p")
+user2 = User.objects.create_user(
+    username="p@p.p", email="p@p.p", password="p", first_name="p", last_name="p"
+)
