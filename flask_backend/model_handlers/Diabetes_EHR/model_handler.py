5c4ab4db15ed64826a02d51677df7fd8318c2ab6
Pat McNeely <pmcneely@gwu.edu>
2024-07-08 16:16:23 -0400

    Add Model Handler structures to the flask backend to support abstraction of model  from server interface

---
+++ flask_backend/model_handlers/Diabetes_EHR/model_handler.py
@@ -0,0 +1,51 @@
+import joblib
+import numpy as np
+import pandas as pd
+
+
+class Diabetes_EHR_Handler:
+    def __init__(self):
+        with open(
+            "./model_handlers/Diabetes_EHR/Diabetes_EHR_classifier_and_features_PM.pickle",
+            "rb",
+        ) as fp:
+            pickled_objects = joblib.load(fp)
+            self.classifier = pickled_objects["classifier"]
+            self.feature_names = pickled_objects["feature_names"]
+        fp.close()
+
+    def make_prediction(self, raw_data):
+
+        headers, data = raw_data[0], np.array([raw_data[1]])
+        patient_data = pd.DataFrame(data, columns=headers)
+        # patient_data = patient_data.reindex(sorted(patient_data.columns), axis=1)
+
+        # Check for missing columns in the new data
+        missing_cols = set(self.feature_names) - set(patient_data.columns)
+        for c in missing_cols:
+            patient_data[c] = 0  # or some other value that makes sense in your context
+
+        # Check for extra columns in the new data
+        extra_cols = set(patient_data.columns) - set(self.feature_names)
+        if extra_cols:
+            print(
+                f"Warning: Found extra columns in the new data that were not in the training data: {extra_cols}"
+            )
+            patient_data = patient_data.drop(columns=extra_cols)
+
+        # Reorder the columns of the new data
+        patient_data_sort = patient_data.reindex(columns=self.feature_names)
+        if not patient_data.equals(patient_data_sort):
+            print(
+                f"Message: patient data features was sorted to match order of model features"
+            )
+
+        prediction = self.classifier.predict(patient_data_sort)[0]
+        return {
+            "result": "This patient {} expected to respond to the intervention based on EHR input".format(
+                "is" if prediction[0] == 1 else "is not"
+            )
+        }
+        # if prediction == "R":
+        #     return "This patient is expected to respond to the intervention based on Metagenomic input"
+        # return "This patient is not expected to respond to the intervention based on Metagenomic input"
---
+++ flask_backend/model_handlers/Diabetes_glycomic/model_handler.py
@@ -0,0 +1,62 @@
+import joblib
+import numpy as np
+import pandas as pd
+
+
+class Diabetes_Glycomic_Handler:
+    def __init__(self):
+        with open("./models/Diabetes_glycomic_v1/diabetes_classifier.pkl", "rb") as fp:
+            self.pickled_model = joblib.load(fp)
+        fp.close()
+
+        # See ../../models/Diabetes_glycomic_v1/{files} for original source
+        self.headers = pd.read_csv(
+            "./models/Diabetes_glycomic_v1/gtc_headers.csv", sep=","
+        )["Header"].to_list()
+
+        self.glycan_dict = {
+            "GP32-33_G67579EM": ["GP32_G67579EM", "GP33_G67579EM"],
+            "GP38-39_G12638YK": ["GP38_G12638YK", "GP39_G12638YK"],
+            "GP41-43_G16933RK": ["GP41_G16933RK", "GP42_G16933RK", "GP43_G16933RK"],
+        }
+
+    def preprocess_input(self, df):
+        # See ../../models/Diabetes_glycomic_v1/{files} for original source
+
+        df.columns = self.headers
+
+        processed_df = pd.DataFrame()
+        for key, value in self.glycan_dict.items():
+            processed_df[key] = df[value].sum(axis=1)
+
+        # Join combined features with original dataframe, drop individual peaks included in combined features
+        df_out = df.join(processed_df)
+        df_out = df_out.drop(
+            columns=[
+                "GP32_G67579EM",
+                "GP33_G67579EM",
+                "GP38_G12638YK",
+                "GP39_G12638YK",
+                "GP41_G16933RK",
+                "GP42_G16933RK",
+                "GP43_G16933RK",
+            ]
+        )
+
+        return df_out
+
+    def make_prediction(self, raw_data):
+
+        headers, data = raw_data[0], np.array([raw_data[1]])
+        df = pd.DataFrame(data, columns=headers)
+        processed_df = self.preprocess_input(df)
+
+        prediction = self.pickled_model.predict(processed_df)
+        return {
+            "result": "This patient {} expected to respond to the intervention based on Metagenomic input".format(
+                "is not" if prediction == 1 else "is"
+            )
+        }
+        # if prediction == "R":
+        #     return "This patient is expected to respond to the intervention based on Metagenomic input"
+        # return "This patient is not expected to respond to the intervention based on Metagenomic input"
