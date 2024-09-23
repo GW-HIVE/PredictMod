16e9cca31c7912e6d15d061b2e5078dd3240be7b
Pat McNeely <pmcneely@gwu.edu>
2024-09-23 16:17:30 -0400

    Include model information files in Django middleware build. Sloppy and hacky, but fast and works.

--- scripts/build_middleware.sh
+++ scripts/build_middleware.sh
@@ -5,5 +5,7 @@ docker rm middleware
 docker rmi predictmod:v0.3.1-middleware
 
 pushd ../django_middleware
+cp -R ../flask_backend .
 docker build -t predictmod:v0.3.1-middleware -f dockerfile .
+rm -rf ./flask_middleware
 popd
