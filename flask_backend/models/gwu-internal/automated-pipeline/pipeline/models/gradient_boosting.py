3b5930e1321c0e981bb6567a795aece07739d315
Pat McNeely <pmcneely@gwu.edu>
2024-12-05 16:29:24 -0500

    WIP - Working (hardcoded) example using the 'MG_Exercise' data. TODOs include updating UI for columns to drop, column with labels, and overall flexibility to other data sets

---
+++ flask_backend/models/gwu-internal/automated-pipeline/pipeline/models/gradient_boosting.py
@@ -0,0 +1,16 @@
+from sklearn.linear_model import LogisticRegression
+
+
+class LogisticRegressionHandler:
+
+    def __init__(self):
+        raise NotImplemented
+
+    def train_model(self):
+        raise NotImplementedError
+
+    def sample_prediction(self):
+        raise NotImplementedError
+
+    def save_model(self):
+        raise NotImplementedError
