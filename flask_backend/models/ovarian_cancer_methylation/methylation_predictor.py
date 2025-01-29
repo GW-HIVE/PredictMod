8f4e902d174be03b1a704ea258e28bd84571fc5b
Miguel Mazumder <72771218+MiguelMazumder@users.noreply.github.com>
2025-01-29 10:17:38 -0500

    Add files via upload

---
+++ flask_backend/models/ovarian_cancer_methylation/methylation_predictor.py
@@ -0,0 +1,17 @@
+import joblib
+import pandas as pd
+import numpy as np
+
+# Load the saved model
+model_data = joblib.load('methylation_model.pkl')
+patient_input = pd.read_excel('methyl_single_patient.xlsx', header=None)
+patient_data = patient_input.iloc[1:, 1:].reset_index(drop=True)
+# patient_features = patient_data.iloc[1:, 0].values
+# Extract the classifier and feature names from the model data
+clf = model_data['model']
+
+# Predict the outcome using the model
+prediction = clf.predict(patient_data.T)
+
+# Output the result
+print(f"The predicted outcome for the patient is: {prediction[0]}")
