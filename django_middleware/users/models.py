02fffd9ee0ec7c365d7cf1989aed6cdc0be6ca62
Pat McNeely <pmcneely@gwu.edu>
2025-02-10 16:58:21 -0500

    WIP - Update users with "trained models" that may be saved, deleted, etc.

--- django_middleware/users/models.py
+++ django_middleware/users/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.utils.translation import gettext_lazy as _
+from django.utils import timezone
 
 # Create your models here.
 
@@ -21,8 +22,19 @@ class SiteUser(models.Model):
     )
 
 
+class AutoDateTimeField(models.DateTimeField):
+    def pre_save(self, model_instance, add):
+        return timezone.now()
+
+
 class TrainedModel(models.Model):
 
     model_name = models.CharField(max_length=255)
+    data_name = models.CharField(max_length=255)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    label_field = models.CharField(max_length=100)
+    drop_fields = models.JSONField(blank=True)
+    to_save = models.BooleanField(default=False)
     serialized_model = models.BinaryField()
     siteuser = models.ForeignKey(SiteUser, on_delete=models.CASCADE, null=True)
