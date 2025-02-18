023b020f3d5fe60d60a78086e74a23d46bfa12dd
Pat McNeely <pmcneely@gwu.edu>
2025-02-18 15:06:23 -0500

    Additional improvements to 'dev_script' for shimming into model inputs/outputs

--- flask_backend/models/gwu-internal/automated-pipeline/dev_script.py
+++ flask_backend/models/gwu-internal/automated-pipeline/dev_script.py
@@ -58,4 +58,30 @@ labels = labels.apply(lambda x: 0 if x == "R" else 1)
 dtc = DecisionTreeClassifierHandler(labels, data)
 results = dtc.train_model()
 new_results = dtc.sample_prediction(new_sample)
+print(f"{'-'*20}{'DTC':^40}{'-'*20}")
 print(new_results)
+print("-"*80)
+
+# Logisitic Regression
+lrc = LogisticRegressionHandler(labels, data)
+results = lrc.train_model()
+new_results = lrc.sample_prediction(new_sample)
+print(f"{'-'*20}{'LRC':^40}{'-'*20}")
+print(new_results)
+print("-"*80)
+
+# Random Forest
+rfc = RandomForestClassifierHandler(labels, data)
+results = rfc.train_model()
+new_results = rfc.sample_prediction(new_sample)
+print(f"{'-'*20}{'RFC':^40}{'-'*20}")
+print(new_results)
+print("-"*80)
+
+# SVM
+svmc = SupportVectorMachineHandler(labels, data)
+results = svmc.train_model()
+new_results = svmc.sample_prediction(new_sample)
+print(f"{'-'*20}{'SVMC':^40}{'-'*20}")
+print(new_results)
+print("-"*80)
