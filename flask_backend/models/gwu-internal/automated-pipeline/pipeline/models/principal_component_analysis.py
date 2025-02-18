bbed4474122988d63baf15fef7f0e0348bd8da10
Pat McNeely <pmcneely@gwu.edu>
2025-02-18 08:59:03 -0500

    WIP - First pass at getting a new point included when using PCA

--- flask_backend/models/gwu-internal/automated-pipeline/pipeline/models/principal_component_analysis.py
+++ flask_backend/models/gwu-internal/automated-pipeline/pipeline/models/principal_component_analysis.py
@@ -4,6 +4,7 @@ from sklearn.decomposition import PCA
 import io
 import matplotlib
 import matplotlib.pyplot as plt
+import numpy as np
 import pandas as pd
 import time
 
@@ -14,38 +15,24 @@ class PCAHandler:
         self.labels = labels
         self.data = data
 
-    def train_model(self):
-        pca = PCA(n_components=3)
-        start = time.time()
-        pca_variance = pca.fit(self.data)
-        reduced_data = pca.fit_transform(self.data)
-        solve_time = time.time() - start
-
-        ### Not needed?
-        # self.pca = pca
-
-        # print(
-        #     f"PCA: Explained variance ratios -\n{pca_variance.explained_variance_ratio_}"
-        # )
-
-        colors = self.labels.apply(lambda x: 0 if x == "R" else 1)
-
-        report_df = pd.DataFrame(
-            reduced_data, columns=["Factor 1", "Factor 2", "Factor 3"]
-        )
-
-        report_df["Labels"] = self.labels
-        report_df["Colors"] = colors
-
-        # print("-" * 80)
-        # print(f"Joined data:\n{report_df}")
-        # print("-" * 80)
+    def create_image(self, new_point=None):
 
         fig = plt.figure(1, figsize=(8, 6))
         ax = fig.add_subplot(111, projection="3d", elev=-150, azim=110)
-        scatter = ax.scatter(
-            reduced_data[:, 0], reduced_data[:, 1], reduced_data[:, 2], c=colors, s=40
-        )
+        data = self.modeled_data["data"]
+        colors = self.modeled_data["colors"]
+
+        if new_point is not None:
+            print(f"Concatenating! Colors is {colors}")
+            print(f"Data is {data}")
+            print(f"New point is {new_point}")
+            np.concatenate((data, new_point), axis=0)
+            # fmt: off
+            np.append(colors, 2)
+            # fmt: on
+
+        scatter = ax.scatter(data[:, 0], data[:, 1], data[:, 2], c=colors, s=40)
+
         ax.set_title("3D PCA Reduction")
         ax.set_xlabel("1st component")
         # ax.xaxis.set_ticklabels([])
@@ -63,7 +50,38 @@ class PCAHandler:
         b64_image = b64encode(buf.getvalue()).decode("utf-8").replace("\n", "")
         # plt.savefig("PCA.png", format="png", bbox_inches="tight")
 
+        return b64_image
+
+    def train_model(self):
+        pca = PCA(n_components=3)
+        start = time.time()
+        pca_variance = pca.fit(self.data)
+        reduced_data = pca.fit_transform(self.data)
+        solve_time = time.time() - start
+
+        ### Not needed?
+        # self.pca = pca
+
+        # print(
+        #     f"PCA: Explained variance ratios -\n{pca_variance.explained_variance_ratio_}"
+        # )
+        colors = self.labels.apply(lambda x: 0 if x == "R" else 1)
+
+        report_df = pd.DataFrame(
+            reduced_data, columns=["Factor 1", "Factor 2", "Factor 3"]
+        )
+
+        report_df["Labels"] = self.labels
+        report_df["Colors"] = colors
+
+        # print("-" * 80)
+        # print(f"Joined data:\n{report_df}")
+        # print("-" * 80)
+
         self.model = pca
+        self.modeled_data = {"data": reduced_data, "colors": colors}
+
+        b64_image = self.create_image()
 
         return {
             "Method": "PCA 3D",
@@ -74,7 +92,12 @@ class PCAHandler:
         }
 
     def sample_prediction(self, new_data):
-        return self.pca.transform(new_data)
+        new_point = self.model.transform(new_data)
+        b64_image = self.create_image(new_point=new_point)
+        return {
+            "image": b64_image,
+            "point": new_point,
+        }
 
     def save_model(self):
         raise NotImplementedError
