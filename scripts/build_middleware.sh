3df0983a3d1e1de2b2f1479477447b0c37ee0996
Pat McNeely <pmcneely@gwu.edu>
2025-02-19 13:04:39 -0500

    Correct typo/remove overly complex & redundant files

--- scripts/build_middleware.sh
+++ scripts/build_middleware.sh
@@ -7,5 +7,5 @@ docker rmi predictmod:v0.3.1-middleware
 pushd ../django_middleware
 cp -R ../flask_backend .
 docker build -t predictmod:v0.3.1-middleware -f dockerfile .
-rm -rf ./flask_middleware
+rm -rf ./flask_backend
 popd
