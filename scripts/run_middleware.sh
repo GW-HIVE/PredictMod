915fc355d9f736206d92596ee23f171f06cef08f
Pat McNeely <pmcneely@gwu.edu>
2025-02-28 12:17:09 -0500

    Add shared directory volume to Dockers

--- scripts/run_middleware.sh
+++ scripts/run_middleware.sh
@@ -7,12 +7,14 @@ if [[ ! $BUILD_ENV ]]; then
     exit 0
 fi
 
+pushd .. 2>&1 > /dev/null
+
 case $BUILD_ENV in
     'shell_testing'|'development')
     echo Build environment found $BUILD_ENV. Launch middleware manually.
     ;;
     'production'|'beta')
-    docker run -d --restart=always --network predictmod  --name middleware predictmod:v0.3.1-middleware
+    docker run -d --restart=always -v $(pwd)/user_data:/user_data --network predictmod  --name middleware predictmod:v0.3.1-middleware
     ;;
     'docker-dev')
     docker run -d --restart=always -p 8000:8000 --name middleware predictmod:v0.3.1-middleware
@@ -21,3 +23,5 @@ case $BUILD_ENV in
     echo Please update your "env.sh" file appropriately.
     ;;
 esac
+
+popd .. 2>&1 > /dev/null
