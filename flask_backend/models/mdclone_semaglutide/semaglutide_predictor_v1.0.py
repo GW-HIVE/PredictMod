b1c02a575a9a01241b6ef05c74579f8c3fd0a408
Miguel Mazumder <72771218+MiguelMazumder@users.noreply.github.com>
2025-04-02 14:39:18 -0400

    Update semaglutide_predictor_v1.0.py

--- flask_backend/models/mdclone_semaglutide/semaglutide_predictor_v1.0.py
+++ flask_backend/models/mdclone_semaglutide/semaglutide_predictor_v1.0.py
@@ -1,39 +1,42 @@
 import pandas as pd
 import joblib
 
-# Load the dictionary from the pickle file
-data = joblib.load('Semaglutide_EHR_classifier_and_features.pickle')
+def predict_patient(patient_file_path = 'single_patient_semaglutide.xlsx',pkl_path = 'Semaglutide_EHR_classifier_and_features.pickle'):
+    # Load the dictionary from the pickle file
+    pkl_path = joblib.load('Semaglutide_EHR_classifier_and_features.pickle')
 
-# Extract the classifier and feature names from the dictionary
-classifier = data['classifier']
-feature_names = data['feature_names']
+    # Extract the classifier and feature names from the dictionary
+    classifier = pkl_path['classifier']
+    feature_names = pkl_path['feature_names']
 
-# Load the single patient data
-file_path = 'single_patient_semaglutide.xlsx'
+    # Read the Excel file
+    try:
+        patient_pkl_path = pd.read_excel(patient_file_path)
+    except Exception as e:
+        print(f'Error reading the file: {e}')
+        exit()
 
-# Read the Excel file
-try:
-    patient_data = pd.read_excel(file_path)
-except Exception as e:
-    print(f'Error reading the file: {e}')
-    exit()
+    # Check for missing columns in the new pkl_path
+    missing_cols = set(feature_names) - set(patient_pkl_path.columns)
+    for c in missing_cols:
+        patient_pkl_path[c] = 0  # or some other value that makes sense in your context
 
-# Check for missing columns in the new data
-missing_cols = set(feature_names) - set(patient_data.columns)
-for c in missing_cols:
-    patient_data[c] = 0  # or some other value that makes sense in your context
+    # Check for extra columns in the new pkl_path
+    extra_cols = set(patient_pkl_path.columns) - set(feature_names)
+    if extra_cols:
+        print(f"Warning: Found extra columns in the new pkl_path that were not in the training pkl_path: {extra_cols}")
+        patient_pkl_path = patient_pkl_path.drop(columns=extra_cols)
 
-# Check for extra columns in the new data
-extra_cols = set(patient_data.columns) - set(feature_names)
-if extra_cols:
-    print(f"Warning: Found extra columns in the new data that were not in the training data: {extra_cols}")
-    patient_data = patient_data.drop(columns=extra_cols)
+    # Reorder the columns of the new pkl_path
+    patient_pkl_path_sort = patient_pkl_path.reindex(columns=feature_names)
+    if not patient_pkl_path.equals(patient_pkl_path_sort):
+        print("Message: patient pkl_path features were sorted to match the order of model features")
 
-# Reorder the columns of the new data
-patient_data_sort = patient_data.reindex(columns=feature_names)
-if not patient_data.equals(patient_data_sort):
-    print("Message: patient data features were sorted to match the order of model features")
+    # Make a prediction
+    prediction = classifier.predict(patient_pkl_path_sort)
+    if prediction == 1:
+        print('Patient is likely to be responsive to Semaglutide treatment')
+    else:
+        print('Patient is unlikely to be responsive to Semaglutide treatment')
 
-# Make a prediction
-prediction = classifier.predict(patient_data_sort)
-print("Prediction:", prediction)
+predict_patient('single_patient_semaglutide.xlsx','Semaglutide_EHR_classifier_and_features.pickle')
