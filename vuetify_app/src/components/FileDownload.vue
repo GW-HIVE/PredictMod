0dca24a6ef89bea565d6eedda08722f86832061d
Pat McNeely <pmcneely@gwu.edu>
2025-04-30 13:31:46 -0400

    More dead code removed

--- vuetify_app/src/components/FileDownload.vue
+++ vuetify_app/src/components/FileDownload.vue
@@ -4,49 +4,10 @@
  -->
 <template>
     <div class="text-center">
-      <!-- <v-row no-gutters justify="center" align="center"> -->
-        <!-- <v-col cols="8"> -->
-        <!-- <v-col> -->
-            <!-- 
-                See `rules` for filtering on file type. 
-                Not super critical at present. 
-             -->
-          <!-- <v-file-input
-            show-size
-            label="Select a PredictMod-formatted xls or xlsx file"
-            type="File"
-            v-model="currentFile"
-          ></v-file-input> -->
-        <!-- </v-col> -->
-        <!--
-            This appears to break when invoked using Vuetify auto-magic 
-            @change="selectFile($event)" 
-        -->
-            
-        <!-- <v-col cols="4" class="pl-2"> -->
-        <!-- <v-container class="justify-center"> -->
-        <!-- <v-row> -->
-        <!-- <v-col class="justify-center">   -->
           <v-btn color="primary" small @click.prevent="download">
             Download example data file
             <!-- <v-icon right dark>mdi-cloud-upload</v-icon> -->
           </v-btn>
-        <!-- </v-col> -->
-        <!-- <v-col>
-          <v-btn right color="success" dark small @click="upload">
-            Submit & Analyze
-          </v-btn>
-        </v-col> -->
-      <!-- </v-row> -->
-    <!-- </v-container> -->
-      <!-- </v-row> -->
-  
-      <!-- <v-alert v-if="message" color="blue-grey" dark>
-        {{ message }}
-      </v-alert> -->
-      <!-- <v-row>
-        <BarChart v-if="showChart" /> 
-      </v-row> -->
     </div>
   </template>
   
--- vuetify_app/src/services/DownloadService.js
+++ vuetify_app/src/services/DownloadService.js
@@ -6,33 +6,10 @@ import Cookies from "js-cookie";
 
 class DownloadFilesService {
   async download(target) {
-    // const formData = new FormData();
 
-    // const urlDest = "/".concat(target, "-sample/");
     const baseURL = import.meta.env.DEV ? import.meta.env.VITE_DEV_MIDDLEWARE_BASE + '/api': "/predictmod/api";
 
     const fullURL = baseURL + `/download/?q=${target}`;
-
-    // const csrftoken = document.querySelector('[name=csrfmiddlewaretoken]').ariaValueMax();
-    // const csrftoken = Cookies.get('csrftoken');
-    // console.log("Got a Cookie: %s", JSON.stringify(csrftoken));
-    // const headers = {
-    //   "Content-type": "application/json",
-    //   "X-CSRFToken": csrftoken,
-    //   // "X-CSRFToken": document.querySelector('[name=csrfmiddlewaretoken]').value(),
-    // }
-
-    // XXX
-    // console.log("---> Downloading file from %s", urlDest);
-    // console.log("---> File contents:\n%s", json);
-    // console.log("---> File TYPE:\n%s", typeof(json));
-    // console.log("XLS? >>>\n%s", JSON.stringify(xlsFile));
-
-    // formData.append("json", json);
-
-    // const downloadTarget = `https://hivelab.tst.biochemistry.gwu.edu/predictmod/api${urlDest}`;
-    // const downloadTarget = `http://hivelab.biochemistry.gwu.edu/predictmod/api${urlDest}`;
-    // const downloadTarget = `http://localhost:8000/predictmod/api/download/q=${urlDest}`;
 
     console.log(`Now downloading from ${fullURL}`);
 
@@ -40,18 +17,6 @@ class DownloadFilesService {
     // console.log("Axios: Returned %s", response.data);
     return response.data;
 
-    // axios.get(fullURL)
-    //   .then(response => {
-    //     const sampleName = target + "_example_data.xlsx";
-    //     const data = JSON.parse(response.data);
-    //     console.log("Received the following: (type %s)\n%s", typeof(response.data), response.data);
-    //     return response.data;
-    //   })
-    //   .catch(error => {
-    //     console.log("---> ERROR!:\n\t%s", JSON.stringify(error));
-    //   })
-
-    // return axiosUtility.get(urlDest);
   }
 }
 
