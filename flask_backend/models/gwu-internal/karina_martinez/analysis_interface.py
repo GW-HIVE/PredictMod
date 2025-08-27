3c9218a18e45b614fce11208eba782183c731715
Pat McNeely <pmcneely@gwu.edu>
2025-08-27 11:53:14 -0400

    Include example analysis in KM scripts

--- flask_backend/models/gwu-internal/karina_martinez/analysis_interface.py
+++ flask_backend/models/gwu-internal/karina_martinez/analysis_interface.py
@@ -126,11 +126,18 @@ def upload():
     target = request.args.get("q", None)
     file_name = request.args.get("file_name", None)
     user = request.args.get("user", None)
-    file_path = os.path.join(SHARED_DIR, f"{user}/{file_name}")
 
-    if file_name is None or user is None:
+    if file_name is None:
         return jsonify({"error": "No file path or user found in request"})
-    app.logger.debug(f"---> Reading data from {file_path}")
+    file_path = ""
+    file_extension = ""
+    if file_name == "example":
+        file_path = os.path.join(MODELS_DIR, DOWNLOAD_LOOKUP[target])
+        file_extension = file_path.split(".")[-1]
+    else:
+        file_path = os.path.join(SHARED_DIR, f"{user}/{file_name}")
+        file_extension = file_name.split(".")[1]
+    # app.logger.debug(f"---> Reading data from {file_path} (found file extension {file_extension})")
 
     file_extension = file_name.split(".")[1]
     if file_extension == "csv":
